d479d3a303f64d5c6658172809c42c777e69acda
Bolade <boladebode@gmail.com>
2020-08-19 11:24:14 +0100

    sidebar modified

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -19,16 +19,19 @@ import Utility from '../../services/UtilityService';
   const toggle = () => setIsOpen(!isOpen);
     return (
         <Fragment>
-            
-          <Navbar className="topbar" expand="md">
+            <div className="logo">Inventory</div>
+                    <ul>
+                        <li><i className="fa fa-search"></i></li>
+                        <li><i className="fa fa-bell"></i></li> 
+                         <li><i className="fa fa-user"></i></li>
+                    </ul>
+          {/* <Navbar className="topbar" expand="md">
           <Container fluid={true}>
         <NavbarBrand href="/">Inventory APP</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
-            {/* <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem> */}
+           
              
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
@@ -45,10 +48,10 @@ import Utility from '../../services/UtilityService';
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
-          {/* <NavbarText>Beedy</NavbarText> */}
+          
         </Collapse>
         </Container>
-      </Navbar> 
+      </Navbar>  */}
         </Fragment>
     )
 }
